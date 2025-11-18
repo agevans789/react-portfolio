421ce8f41a478481608d8a4b910791e192c6e3bb
Ann Grace Evans <agevans789@gmail.com>
2025-11-18 14:55:43 -0500

    add react link to project card

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,15 +1,18 @@
 import react from 'react';
+import {Link} from 'react-router-dom'
 
 const ProjectCard = ({project, addNewProject}) => {
     return (
-        <div class="card">
-            <img src=""></img>
-            <div className="card-body">
-                <h5 class="card-title">{project.name}</h5>
-                <a href="" class="card-link">{project.url}</a>
-                <p class="card-text">{project.description}</p>
+        <Link to={project.url}>
+            <div class="card">
+                <img src={project.image}></img>
+                <div className="card-body">
+                    <h5 class="card-title">{project.name}</h5>
+                    <a href={project.url} class="card-link"></a>
+                    <p class="card-text">{project.description}</p>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 };
 
