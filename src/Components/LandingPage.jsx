ecd23a0cc8acfa32334f8b93b3d6b0b7110024a3
Ann Grace Evans <agevans789@gmail.com>
2025-11-22 16:29:50 -0500

    fixed landing page

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import ProjectCard from './ProjectCard';
 import {useState} from 'react';
 
-const LandingPage = ({projects, handleSearch}) => {
+const LandingPage = ({projects, onHandleSearch}) => {
     // add search input area to handleSearch
     return (
         <div className="LandingPage">
-            <form type="search" onSearch={handleSearch}>
+            <form type="search" onSearch={onHandleSearch}>
                 <input type="text">Search</input>
                 <button class="btn btn-primary" type="submit">Search</button>
                 {projects.map((project) => (
-                    <ProjectCard key={project.name} project={project} handleSearch={handleSearch}/>
+                    <ProjectCard key={project.name} project={project}/>
                 ))}
             </form>
         </div>
