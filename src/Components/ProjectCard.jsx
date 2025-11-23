8309b202798ac47db93866ad9615571b86269770
Ann Grace Evans <agevans789@gmail.com>
2025-11-22 21:19:07 -0500

    css fixes

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import react from 'react';
 const ProjectCard = ({project, onHandleDelete, onHandleAddNewProjects}) => {
     return (
         <div className="card">
-                <img src={project.image}></img>
+                <img src={project.image} alt={project.name} id='card-image'></img>
             <div className="card-body">
                     <h5 className="card-title">{project.name}</h5>
                     <p className="card-text">{project.description}</p>
--- src/projects.js
+++ src/projects.js
@@ -3,17 +3,24 @@ import { v4 } from 'uuid';
 const projects = [
     {
         id: v4(),
-        name: "CSS Layouts Lab",
-        url: "x",
-        image: "x",
-        description: "x"
+        name: "Hogwild Lab",
+        url: "https://github.com/agevans789/react-hogwild-vite.git",
+        image: "https://upload.wikimedia.org/wikipedia/commons/3/3e/Pig_farm_Vampula_1.jpg",
+        description: "Application that allows users to view and manage a list of hogs with various attributes."
     },
     {
         id: v4(),
         name: "React Forms Vite Lab",
-        url: "x",
-        image: "x",
-        description: "x"
+        url: "https://github.com/agevans789/React-Forms-Vite-Lab.git",
+        image: "https://blog.openreplay.com/images/how-to-build-your-react-app-using-vite/images/hero.png",
+        description: "A React application demonstrating form handling and state management using Vite as the build tool."
+    },
+    {
+        id: v4(),
+        name: "Dynamic Color Clock Lab",
+        url: "https://github.com/agevans789/dynamic-color-clock.git",
+        image: "https://rlv.zcache.com/color_wheel_and_time_teaching_clock-r257db165112c48d0a3cf89f6e30645c7_s0ysk_8byvr_644.webp",
+        description: "A digital clock that changes color based on the current time using HTML, CSS, and JavaScript."
     }
 ];
 
